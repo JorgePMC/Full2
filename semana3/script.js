126d6c50642dcc9a046e76d1b4e1153b456edbe0
JorgePMC <formalidad.jpg@gmail.com>
2026-08-25 13:08:55 -0400

    mas datos en la semana 3

---
+++ semana3/script.js
@@ -0,0 +1,33 @@
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const taskList= document.getElementById('taskList');
+
+addBtn.addEventListener('click', () => {
+    const text = taskInput.ariaValueMax.trim();
+
+    if(text === ''){
+        alert('Debes ingresar una tarea');
+        return;
+    }
+
+    const li = document.createElement('li');
+
+    const span = document.createElement('span');
+    span.textContent = text;
+
+    span.addEventListener('click', () => {
+        span.classList.toggle('done');
+    })
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'Eliminar';
+    deleteBtn.addEventListener('click', () => {
+        li.remove();
+    });
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+});
